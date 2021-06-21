db829276030cb4d523868e8e53b79cf95b3864c8
Roi malka <roimalka10@gmail.com>
2021-06-22 01:57:07 +0300

    changed the loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,17 +1,14 @@
 import React from "react";
 import FadeLoader from "react-spinners/FadeLoader";
-import { css } from "@emotion/react";
+import { Icon } from "@iconify/react";
+import locationIcon from "@iconify/icons-mdi/fire-alert";
 
 const color = "white";
 
 const Loader = () => {
   return (
     <div className="loader">
-      <FadeLoader
-        className={css`
-          border-color: ${color};
-        `}
-      />
+      <Icon icon={locationIcon} className="icon" />
       <h1>Getting Data</h1>
     </div>
   );
